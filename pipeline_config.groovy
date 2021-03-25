207ac0c4e6b5415e1a30a72c23338e2bd0530d01
WilliamLu2019 <dayong.lu@paradyme.us>
2021-03-25 12:03:21 -0400

    add pipeline_config

---
+++ pipeline_config.groovy
@@ -0,0 +1,10 @@
+libraries {
+    maven
+}
+keywords {
+    projectName = 'speed'
+    imageName = 'java-api-management'
+    argoAppName = 'api-management-server'
+    imageTagFlag = 'apimanagement.image.tag'
+    releaseVersion = 'RELEASE-1.1.0'
+}
